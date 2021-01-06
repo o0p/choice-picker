1d4ec30ccb79ebb775f4079b505f4f4e2da7b38a
Russell <russelldev@protonmail.com>
2021-01-06 20:28:45 +0000

    Add texarea listener

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const tagsEL = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+});
+
+function createTags(input) {
+    console.log(input);
+}
